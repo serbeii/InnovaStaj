be593b92add5b7a3568695e7bbaadfb721adba53
cserimer <cserimer@innova.com.tr>
2024-01-23 17:35:54 +0300

    generated createDB.sql, updated application.properties disables SecurityAutoConfiguration.class

--- database scripts/createDB.sql
+++ database scripts/createDB.sql
@@ -1,7 +1,5 @@
-CREATE DATABASE deneme;
-USE deneme;
-CREATE TABLE user(
+CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    username UNIQUE VARCHAR(100) NOT NULL,
+    username VARCHAR(100) UNIQUE NOT NULL,
     password CHAR(60) NOT NULL
 );
--- src/main/java/serbeii/staj/StajApplication.java
+++ src/main/java/serbeii/staj/StajApplication.java
@@ -2,8 +2,9 @@ package serbeii.staj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class StajApplication {
 
 	public static void main(String[] args) {
